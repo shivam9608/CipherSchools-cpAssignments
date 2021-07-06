4945def65b72d6b83e6740de4bd9709ca945bf9d
SHIVAM KUMAR SINGH <63108649+shivam9608@users.noreply.github.com>
2021-07-06 16:32:50 +0530

    Create peakElementinArray.cpp

---
+++ Assignment-1/peakElementinArray.cpp
@@ -0,0 +1,35 @@
+// Shivam Kumar Singh -- 22 June
+// Question 2. Peak Element in Array
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int findPeakUtil(int arr[], int low, int high, int n) 
+{
+    int mid = low + (high - low) / 2;
+    if ((mid == 0 || arr[mid - 1] <= arr[mid]) &&
+        (mid == n - 1 || arr[mid + 1] <= arr[mid]))
+        return mid;
+     else if (mid > 0 && arr[mid - 1] > arr[mid])
+        return findPeakUtil(arr, low, (mid - 1), n);
+
+    else
+        return findPeakUtil(
+            arr, (mid + 1), high, n);
+}
+
+
+int findPeak(int arr[], int n)
+{
+    return findPeakUtil(arr, 0, n - 1, n);
+}
+
+
+int main()
+{
+    int arr[] = { 1, 3, 20, 4, 1, 0 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    cout << "Index of a peak point is "
+         << findPeak(arr, n);
+    return 0;
+}
