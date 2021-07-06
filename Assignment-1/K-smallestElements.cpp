d015229b0daa7b3002966f6135b9307d57fb68b2
SHIVAM KUMAR SINGH <63108649+shivam9608@users.noreply.github.com>
2021-07-06 16:55:06 +0530

    Create K-smallestElements.cpp

---
+++ Assignment-1/K-smallestElements.cpp
@@ -0,0 +1,20 @@
+//Question 8. K smallest elements
+
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int kthSmallest(int arr[], int n, int k)
+{
+    // Sort the given array
+    sort(arr, arr + n);
+    return arr[k - 1];
+}
+
+int main()
+{
+    int arr[] = { 12, 3, 5, 7, 19 };
+    int n = sizeof(arr) / sizeof(arr[0]), k = 2;
+    cout << "K'th smallest element is " << kthSmallest(arr, n, k);
+    return 0;
+}
